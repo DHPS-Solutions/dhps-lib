5e07973f5b3a6272db696ca7950ce7841a35a8b7
Nicolai Brand <nicolaihbrand@gmail.com>
2023-02-17 13:48:09 +0100

    fix hashmap get_from_bucket() wrong return type (#24)

--- examples/hashmap_example.c
+++ examples/hashmap_example.c
@@ -30,7 +30,8 @@ void stress_test()
 
 int main()
 {
-    stress_test();
+    /* puts and later gets a bunch of entries in a hashmap */
+    //stress_test();
 
     struct hashmap_t map;
     hashmap_init(&map);
@@ -42,14 +43,28 @@ int main()
     char *a = "hello";
     hashmap_put(&map, a, strlen(a) + 1, &x, 1);
     char *b = "hellO";
-    hashmap_put(&map, b, strlen(b) + 1, &y, 1);
+    hashmap_sput(&map, b, &y, 1);
     char *c = "hello.";
-    hashmap_put(&map, c, strlen(c) + 1, &z, 1);
+    hashmap_sput(&map, c, &z, 1);
 
     int *X = hashmap_get(&map, a, strlen(a) + 1);
     int *Y = hashmap_get(&map, b, strlen(b) + 1);
-    int *Z = hashmap_get(&map, c, strlen(c) + 1);
+    int *Z = hashmap_sget(&map, c);
     assert(*X == x && *Y == y && *Z == z);
+
+    assert(hashmap_len(&map) == 3);
+    bool rc;
+    /* removing an item */
+    rc = hashmap_rm(&map, a, strlen(a) + 1);
+    assert(rc == true);
+    assert(hashmap_len(&map) == 2);
+
+    /* removing an item that is already removed */
+    rc = hashmap_rm(&map, a, strlen(a) + 1);
+    assert(rc == false);
+    X = hashmap_sget(&map, a);
+    assert(X == NULL);
+    assert(hashmap_len(&map) == 2);
 
     hashmap_free(&map);
 }
