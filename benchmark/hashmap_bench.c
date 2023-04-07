32a3348f6cc41ac293c428e7506648e3111570f5
Nicolai Brand <nicolaihbrand@gmail.com>
2023-04-07 14:01:39 +0200

    add hashmap benchmark (#48)
    * add hashmap benchmark
    
    * log to store measurements

---
+++ benchmark/hashmap_bench.c
@@ -0,0 +1,89 @@
+/* Written by Nicolai H. Brand (lytix.dev), 2023 */
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include "../hashmap.h"
+
+#define N 100000
+
+int main()
+{
+    char *table[] = {
+        "alignas",
+        "alignof",
+        "auto",
+        "bool",
+        "break",
+        "case",
+        "char",
+        "const",
+        "constexpr",
+        "continue",
+        "default",
+        "do",
+        "double",
+        "else",
+        "enum",
+        "extern",
+        "false",
+        "float",
+        "for",
+        "goto",
+        "if",
+        "inline",
+        "int",
+        "long",
+        "nullptr",
+        "register",
+        "restrict",
+        "return",
+        "short",
+        "signed",
+        "sizeof",
+        "static",
+        "static_assert",
+        "struct",
+        "switch",
+        "thread_local",
+        "true",
+        "typedef",
+        "typeof",
+        "typeof_unqual",
+        "union",
+        "unsigned",
+        "void",
+        "volatile",
+        "while",
+    };
+
+    struct hashmap_t map;
+    hashmap_init(&map);
+    int len = sizeof(table) / sizeof(table[0]);
+    for (int i = 0; i < len; i++) {
+        hashmap_sput(&map, table[i], &i, 4, true);
+    }
+
+    assert(map.len == len);
+
+    clock_t start = clock();
+    for (int i = 0; i < N; i++) {
+        for (int i = 0; i < len; i++) {
+            int *v = hashmap_sget(&map, table[i]);
+            assert(i == *v);
+        }
+    }
+    clock_t end = clock();
+
+    hashmap_free(&map);
+
+    double cpu_time_used = ((double) (end - start) * 1000) / CLOCKS_PER_SEC;
+    double per = (cpu_time_used * 1000 / (double)(len * N));
+    printf("Getting %d items took %fms\n", len * N, cpu_time_used);
+    printf("Time per get: %fus\n", per);
+
+#ifdef WRITE_TO_LOG
+    FILE *fp = fopen("hm_log.csv", "w");
+    fprintf(fp, "%s,%f,%f,0\n", __DATE__, cpu_time_used, per);
+    fclose(fp)
+#endif
+}
